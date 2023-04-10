91923c18b57a94a0975b1d842f4d1a225bfb398a
yukimemi <yukimemi@gmail.com>
2023-04-10 09:42:55 +0900

    :zap: Add some plugins.

--- init.lua
+++ init.lua
@@ -16,16 +16,13 @@ return {
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
-
   -- Set colorscheme to use
   colorscheme = "astrodark",
-
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
     underline = true,
   },
-
   lsp = {
     -- customize lsp formatting options
     formatting = {
@@ -53,7 +50,6 @@ return {
       -- "pyright"
     },
   },
-
   -- Configure require("lazy").setup() options
   lazy = {
     defaults = { lazy = true },
@@ -64,7 +60,6 @@ return {
       },
     },
   },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
@@ -81,5 +76,23 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.api.nvim_create_augroup("MyAutoCmd", { clear = true })
+    vim.api.nvim_create_autocmd("UIEnter", {
+      group = "MyAutoCmd",
+      pattern = "*",
+      once = true,
+      callback = function()
+        vim.opt.mouse = "a"
+        if vim.fn.exists ":GuiFont" > 0 then vim.cmd [[GuiFont! HackGen Console NF:h10]] end
+        if vim.fn.exists ":GuiTabline" > 0 then vim.cmd [[GuiTabline 0]] end
+        if vim.fn.exists ":GuiPopupmenu" > 0 then vim.cmd [[GuiPopupmenu 0]] end
+        if vim.fn.exists ":GuiScrollBar" > 0 then vim.cmd [[GuiScrollBar 0]] end
+        if vim.fn.exists ":GuiWindowOpacity" > 0 then vim.cmd [[GuiWindowOpacity 0.95]] end
+
+        -- Right Click Context Menu (Copy-Cut-Paste)
+        vim.keymap.set({ "n", "i" }, "<RightMouse>", "<cmd>call GuiShowContextMenu()<CR>")
+        vim.keymap.set({ "x", "s" }, "<RightMouse>", "<cmd>call GuiShowContextMenu()<CR>gv")
+      end,
+    })
   end,
 }

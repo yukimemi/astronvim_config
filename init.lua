67f7fcb8e748df6372d44dd4ef81ba07b616353d
yukimemi <yukimemi@gmail.com>
2023-05-03 13:33:16 +0900

    :zap: Add tsserver config.

--- init.lua
+++ init.lua
@@ -64,6 +64,11 @@ return {
         opts.single_file_support = false
         return opts
       end,
+      tsserver = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern "package.json"
+        opts.single_file_support = false
+        return opts
+      end,
       lemminx = {
         settings = {
           xml = {
